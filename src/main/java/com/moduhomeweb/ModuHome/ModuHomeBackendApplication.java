7bd5a21abea56495024323841e10f4bd9e1211e2
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 19:13:09 +0200

    initial commit

---
+++ src/main/java/com/moduhomeweb/ModuHome/ModuHomeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moduhomeweb.ModuHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuHomeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuHomeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moduhomeweb/ModuHome/ModuHomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moduhomeweb.ModuHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuHomeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
