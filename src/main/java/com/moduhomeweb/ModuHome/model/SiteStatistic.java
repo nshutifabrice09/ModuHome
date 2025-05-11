93f80a86eb5c1e3d4ac6055c067b982f72f880ea
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 10:03:04 +0200

    annotation-model-commit

--- src/main/java/com/moduhomeweb/ModuHome/model/SiteStatistic.java
+++ src/main/java/com/moduhomeweb/ModuHome/model/SiteStatistic.java
@@ -1,9 +1,22 @@
 package com.moduhomeweb.ModuHome.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
+@Data
+@Builder
+@Entity
+@Table(name = "site_statistics")
+@NoArgsConstructor
+@AllArgsConstructor
 public class SiteStatistic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int totalUsers;
     private int totalBuilders;
