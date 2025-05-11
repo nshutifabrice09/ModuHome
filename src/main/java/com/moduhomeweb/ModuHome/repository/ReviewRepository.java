a0b6647a7211f54eb0d144c680159316e77c24b8
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 13:49:16 +0200

    repo-commit

--- src/main/java/com/moduhomeweb/ModuHome/repository/ReviewRepository.java
+++ src/main/java/com/moduhomeweb/ModuHome/repository/ReviewRepository.java
@@ -1,2 +1,9 @@
-package com.moduhomeweb.ModuHome.repository;public interface ReviewRepository {
+package com.moduhomeweb.ModuHome.repository;
+
+import com.moduhomeweb.ModuHome.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
--- src/main/java/com/moduhomeweb/ModuHome/repository/RoleRepository.java
+++ src/main/java/com/moduhomeweb/ModuHome/repository/RoleRepository.java
@@ -1,2 +1,9 @@
-package com.moduhomeweb.ModuHome.repository;public interface RoleRepository {
+package com.moduhomeweb.ModuHome.repository;
+
+import com.moduhomeweb.ModuHome.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository <Role, Long> {
 }
