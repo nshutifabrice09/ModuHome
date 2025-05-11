9e65d26989b2c0bad82ad339566427d22ee7333b
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 10:04:29 +0200

    annotation-model-commit

--- src/main/java/com/moduhomeweb/ModuHome/model/SupportTicket.java
+++ src/main/java/com/moduhomeweb/ModuHome/model/SupportTicket.java
@@ -1,12 +1,21 @@
 package com.moduhomeweb.ModuHome.model;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
+@Data
+@Builder
+@Entity
+@Table(name = "support_tickets")
+@NoArgsConstructor
+@AllArgsConstructor
 public class SupportTicket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String subject;
     private String message;
--- src/main/java/com/moduhomeweb/ModuHome/model/Tag.java
+++ src/main/java/com/moduhomeweb/ModuHome/model/Tag.java
@@ -1,7 +1,20 @@
 package com.moduhomeweb.ModuHome.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@Table(name = "tags")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
 }
