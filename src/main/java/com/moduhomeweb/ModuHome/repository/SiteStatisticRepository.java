e922a749c81f68370fc556f32dfebbcf3eac0419
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 13:26:29 +0200

    repo-commit

--- src/main/java/com/moduhomeweb/ModuHome/repository/SiteStatisticRepository.java
+++ src/main/java/com/moduhomeweb/ModuHome/repository/SiteStatisticRepository.java
@@ -1,2 +1,9 @@
-package com.moduhomeweb.ModuHome.repository;public interface SiteStatisticRepository {
+package com.moduhomeweb.ModuHome.repository;
+
+import com.moduhomeweb.ModuHome.model.SiteStatistic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SiteStatisticRepository extends JpaRepository<SiteStatistic, Long> {
 }
--- src/main/java/com/moduhomeweb/ModuHome/repository/SupportTicketRepository.java
+++ src/main/java/com/moduhomeweb/ModuHome/repository/SupportTicketRepository.java
@@ -1,2 +1,9 @@
-package com.moduhomeweb.ModuHome.repository;public interface SupportTicketRepository {
+package com.moduhomeweb.ModuHome.repository;
+
+import com.moduhomeweb.ModuHome.model.SupportTicket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupportTicketRepository extends JpaRepository<SupportTicket, Long> {
 }
