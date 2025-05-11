2d75f6aa7f8be7368bddbd33ce903227b5449cda
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 09:56:12 +0200

    annotation-model-commit

--- src/main/java/com/moduhomeweb/ModuHome/model/MessageThread.java
+++ src/main/java/com/moduhomeweb/ModuHome/model/MessageThread.java
@@ -1,2 +1,18 @@
-package com.moduhomeweb.ModuHome.model;public class MessageThread {
+package com.moduhomeweb.ModuHome.model;
+
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDateTime;
+
+public class MessageThread {
+    private Long id;
+    private String message;
+    private boolean isRead;
+    private LocalDateTime sentAt;
+
+    @ManyToOne
+    private User sender;
+
+    @ManyToOne
+    private User receiver;
 }
--- src/main/java/com/moduhomeweb/ModuHome/model/SiteStatistic.java
+++ src/main/java/com/moduhomeweb/ModuHome/model/SiteStatistic.java
@@ -1,2 +1,13 @@
-package com.moduhomeweb.ModuHome.model;public class SiteStatistic {
+package com.moduhomeweb.ModuHome.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class SiteStatistic {
+    private Long id;
+    private int totalUsers;
+    private int totalBuilders;
+    private int totalBlueprints;
+    private BigDecimal totalRevenue;
+    private LocalDate snapshotDate;
 }
