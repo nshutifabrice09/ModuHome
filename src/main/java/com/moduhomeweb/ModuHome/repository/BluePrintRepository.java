707c508db8cdfac38d51a0d46205f9706e05c18e
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-12 23:18:11 +0200

    model-update

--- src/main/java/com/moduhomeweb/ModuHome/repository/BluePrintRepository.java
+++ src/main/java/com/moduhomeweb/ModuHome/repository/BluePrintRepository.java
@@ -1,2 +1,9 @@
-package com.moduhomeweb.ModuHome.repository;public class BluePrintRepository {
+package com.moduhomeweb.ModuHome.repository;
+
+import com.moduhomeweb.ModuHome.model.Blueprint;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BluePrintRepository extends JpaRepository <Blueprint, Long> {
 }
